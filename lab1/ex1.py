8449d82a37638b1a143e3693d72c1136de36b655
pbartkowicz <pat.bartkowicz@gmail.com>
2020-03-28 21:45:23 +0100

    Update to allow new lines between numbers

--- lab1/ex1.py
+++ lab1/ex1.py
@@ -1,5 +1,5 @@
 class Calculator:
 
     def add(self, numbers: str):
-        split_numbers = list(filter(lambda x: x != "", numbers.split(",")))
+        split_numbers = list(filter(lambda x: x != "", numbers.replace(",", "\n").split("\n")))
         return sum([int(x) for x in split_numbers])
--- tests/lab1/test_ex1.py
+++ tests/lab1/test_ex1.py
@@ -15,6 +15,11 @@ class TestStringCalculator(unittest.TestCase):
         self.assertEqual(c.add("1,2,3"), 6)
         self.assertEqual(c.add("10,90,10,20"), 130)
 
+    def test_add_numbers_using_new_line_delimiter(self):
+        c = Calculator()
+        self.assertEqual(c.add("1\n2,3"), 6)
+        self.assertEqual(c.add("10\n90,10\n20"), 130)
+
 
 if __name__ == '__main__':
     unittest.main()
