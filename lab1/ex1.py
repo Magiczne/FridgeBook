86392d9e1f9fed1868887e76668a6a8f72539aba
pbartkowicz <pat.bartkowicz@gmail.com>
2020-03-28 21:35:45 +0100

    Update method to support an unknown amount of numbers

--- lab1/ex1.py
+++ lab1/ex1.py
@@ -2,8 +2,4 @@ class Calculator:
 
     def add(self, numbers: str):
         split_numbers = list(filter(lambda x: x != "", numbers.split(",")))
-        if len(split_numbers) == 0:
-            return 0
-        if len(split_numbers) == 1:
-            return int(split_numbers[0])
-        return int(split_numbers[0]) + int(split_numbers[1])
+        return sum([int(x) for x in split_numbers])
