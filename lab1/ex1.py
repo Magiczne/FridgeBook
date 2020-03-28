47cb484e1a8423cd2c2b984e841512d5a123d0c0
pbartkowicz <pat.bartkowicz@gmail.com>
2020-03-28 22:44:29 +0100

    Update to support different delimiters

--- lab1/ex1.py
+++ lab1/ex1.py
@@ -1,5 +1,13 @@
+import re
+
+
 class Calculator:
 
     def add(self, numbers: str):
-        split_numbers = list(filter(lambda x: x != "", numbers.replace(",", "\n").split("\n")))
+        delimiters = [",", "\n"]
+        if numbers.startswith("//"):
+            split = numbers.split("\n", 2)
+            delimiters.append(split[0].replace("//", ""))
+            numbers = split[1]
+        split_numbers = list(filter(lambda x: x != "", re.split("|".join(delimiters), numbers)))
         return sum([int(x) for x in split_numbers])
--- tests/lab1/test_ex1.py
+++ tests/lab1/test_ex1.py
@@ -20,6 +20,11 @@ class TestStringCalculator(unittest.TestCase):
         self.assertEqual(c.add("1\n2,3"), 6)
         self.assertEqual(c.add("10\n90,10\n20"), 130)
 
+    def test_add_numbers_using_custom_delimiter(self):
+        c = Calculator()
+        self.assertEqual(c.add("//;\n1;2"), 3)
+        self.assertEqual(c.add("//;\n1;2;4"), 7)
+
 
 if __name__ == '__main__':
     unittest.main()
