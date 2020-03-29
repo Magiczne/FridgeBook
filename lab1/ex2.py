e6a35f0f949cc0a1ee454158e492b25cb53f1358
pbartkowicz <pat.bartkowicz@gmail.com>
2020-03-29 12:20:01 +0200

    Update function to raise exception when number is incorrect

--- lab1/ex2.py
+++ lab1/ex2.py
@@ -1,5 +1,8 @@
 def number_of_bits(number: str):
     if number == "":
         return 0
-    bits = [ones for ones in bin(int(number))[2:] if ones == "1"]
+    number = int(number)
+    if number < 0 or number > 255:
+        raise Exception("number must be between 0 and 255, received: " + str(number))
+    bits = [ones for ones in bin(number)[2:] if ones == "1"]
     return len(bits)
--- tests/lab1/test_ex2.py
+++ tests/lab1/test_ex2.py
@@ -12,6 +12,14 @@ class TestBitsCalculator(unittest.TestCase):
         self.assertEqual(number_of_bits("15"), 4)
         self.assertEqual(number_of_bits("255"), 8)
 
+    def test_incorrect_number(self):
+        with pytest.raises(Exception, match="number must be between 0 and 255, received: -1"):
+            number_of_bits("-1")
+        with pytest.raises(Exception, match="number must be between 0 and 255, received: 256"):
+            number_of_bits("256")
+        with pytest.raises(Exception, match="number must be between 0 and 255, received: -13"):
+            number_of_bits("-13")
+
 
 if __name__ == '__main__':
     unittest.main()
