d76aacdf8b847f632e2a6ee3e365b9b60a563888
pbartkowicz <pat.bartkowicz@gmail.com>
2020-03-29 12:40:43 +0200

    Update to support receiving many numbers

--- lab1/ex2.py
+++ lab1/ex2.py
@@ -1,8 +1,10 @@
-def number_of_bits(number: str):
-    if number == "":
-        return 0
-    number = int(number)
-    if number < 0 or number > 255:
-        raise Exception("number must be between 0 and 255, received: " + str(number))
-    bits = [ones for ones in bin(number)[2:] if ones == "1"]
-    return len(bits)
+def number_of_bits(numbers: str):
+    numbers = numbers.split(";")
+    numbers = [int(x) for x in numbers if x != ""]
+    incorrect_numbers = [x for x in numbers if x < 0 or x > 255]
+    if len(incorrect_numbers) != 0:
+        raise Exception("number must be between 0 and 255, received: " + ",".join([str(x) for x in incorrect_numbers]))
+    bits = 0
+    for number in numbers:
+        bits += len([ones for ones in bin(number)[2:] if ones == "1"])
+    return bits
--- tests/lab1/test_ex2.py
+++ tests/lab1/test_ex2.py
@@ -20,6 +20,19 @@ class TestBitsCalculator(unittest.TestCase):
         with pytest.raises(Exception, match="number must be between 0 and 255, received: -13"):
             number_of_bits("-13")
 
+    def test_correct_numbers(self):
+        self.assertEqual(number_of_bits("1;2"), 2)
+        self.assertEqual(number_of_bits("4;6;7"), 6)
+        self.assertEqual(number_of_bits("0;15;255"), 12)
+
+    def test_incorrect_numbers(self):
+        with pytest.raises(Exception, match="number must be between 0 and 255, received: -1"):
+            number_of_bits("-1;1")
+        with pytest.raises(Exception, match="number must be between 0 and 255, received: -13,-9"):
+            number_of_bits("-13;0;-9;1")
+        with pytest.raises(Exception, match="number must be between 0 and 255, received: -2"):
+            number_of_bits("1;-2")
+
 
 if __name__ == '__main__':
     unittest.main()
