cf97f82d4de5b772f0d84203910b8f21ce9a5f2d
Marcin Wrotecki <marcin.wrotecki.97@gmail.com>
2020-03-31 20:29:41 +0200

    [Green] Improve add function. Pass all tests

--- calculator.py
+++ calculator.py
@@ -9,5 +9,12 @@ class Calculator:
         split_numbers = numbers.replace("\n", ',').split(",")
         split_numbers = list(filter(None, split_numbers))
         split_numbers = list(map(int, split_numbers))
+        negative_numbers = list()
+        for x in split_numbers:
+            if x < 0:
+                negative_numbers.append(x)
+
+        if len(negative_numbers) > 0:
+            raise Exception("negatives not allowed: " + ",".join([str(x) for x in negative_numbers]))
 
         return sum(split_numbers)
