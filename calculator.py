8c184f87f918718d8c745f09207c61c340a725bb
Marcin Wrotecki <marcin.wrotecki.97@gmail.com>
2020-03-31 19:46:28 +0200

    [REFACTOR] Refactor add method

--- calculator.py
+++ calculator.py
@@ -3,7 +3,5 @@ class Calculator:
         split_numbers = numbers.split(',')
         split_numbers = list(filter(None, split_numbers))
         split_numbers = list(map(int, split_numbers))
-        sum=0
-        for i in split_numbers:
-            sum+=i
-        return sum
+
+        return sum(split_numbers)
