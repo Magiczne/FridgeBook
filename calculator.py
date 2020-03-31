85ce25615f1342e9638354e071c19641fc7d7509
Marcin Wrotecki <marcin.wrotecki.97@gmail.com>
2020-03-31 23:43:57 +0200

    [GREEN] Improve add method. Pass new test

--- calculator.py
+++ calculator.py
@@ -1,12 +1,13 @@
 class Calculator:
     def add(self, numbers: str) -> int:
 
-        if numbers.startswith("//"):
-            delimiter = ["//", ";"]
-            for i in delimiter:
-                numbers = numbers.replace(i, ",")
+        numbers = numbers.strip()
+        delimiter = ["//", ";", '*', ']', '[', ',', "\n"]
+        for i in delimiter:
+            numbers = numbers.replace(i, " ")
+        numbers = " ".join(numbers.split())
 
-        split_numbers = numbers.replace("\n", ',').split(",")
+        split_numbers = numbers.split(" ")
         split_numbers = list(filter(None, split_numbers))
         split_numbers = list(map(int, split_numbers))
 
