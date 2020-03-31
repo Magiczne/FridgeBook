ce235eeb719f45ec25ace2dbee536319ba9259dc
Marcin Wrotecki <marcin.wrotecki.97@gmail.com>
2020-03-31 19:41:39 +0200

    [RED] Add test_add_adds_up_to_any_number_when_string_is_valid, rename previous test to the pep8 standard

--- calculator_test.py
+++ calculator_test.py
@@ -4,9 +4,20 @@ import calculator
 
 class MyTestCase(unittest.TestCase):
 
-    def test_Add_AddsUpToTwoNumber_WhenStringIsValid(self):
+    def test_Add_adds_up_to_two_number_when_string_is_valid(self):
         data = ["", "1", "1,2"]
         expected = [0, 1, 3]
+        # Arrange
+        calc = calculator.Calculator()
+        # Act
+        for (inp, out) in zip(data, expected):
+            result = calc.add(inp)
+            # Assert
+            self.assertEqual(out, result)
+
+    def test_add_adds_up_to_any_number_when_string_is_valid(self):
+        data = ["1,2,3", "10,90,10,20"]
+        expected = [6, 130]
         # Arrange
         calc = calculator.Calculator()
         # Act
