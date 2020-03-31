df3cece2b80e9907a73a3e5bf36af3d0a3615293
filip <filip.raszka3@gmail.com>
2020-03-31 20:29:46 +0200

    calculator multiple args and mixed delimiters

--- src/cw1.py
+++ src/cw1.py
@@ -1,7 +1,13 @@
+import re
+
+
 class Calculator(object):
 
+    def __init__(self):
+        self.split_regex = '|'.join([',', '\n'])
+
     def add(self, numbers: str) -> int:
-        split_numbers = numbers.split(',')
+        split_numbers = re.split(self.split_regex, numbers)
         if len(split_numbers) == 1 and not split_numbers[0]:
             return 0
         else:
--- tests/cw1.py
+++ tests/cw1.py
@@ -4,13 +4,40 @@ from src.cw1 import Calculator
 
 class TestStringCalculator(unittest.TestCase):
 
-    def test_add_up_to_two_numbers(self):
+    def test_add_up_to_two_numbers_valid(self):
         calculator = Calculator()
 
         data = {
             'empty string': ["", 0],
             'one number': ["1", 1],
             'two numbers': ["1,2", 3]
+        }
+
+        for name, (arg, expected) in data.items():
+            with self.subTest():
+                result = calculator.add(arg)
+                self.assertEqual(expected, result)
+
+    def test_add_multiple_args_valid(self):
+        calculator = Calculator()
+
+        data = {
+            '1': ["1,2,5,8,12,3", 31],
+            '2': ["1,2,2,0,12,3", 20]
+        }
+
+        for name, (arg, expected) in data.items():
+            with self.subTest():
+                result = calculator.add(arg)
+                self.assertEqual(expected, result)
+
+    def test_add_different_delimiters_valid(self):
+        calculator = Calculator()
+
+        data = {
+            'only commas': ["1,2,3,4,5", 15],
+            'only new lines ': ["1\n2\n3\n4\n5", 15],
+            'commas and new lines': ["1,2\n3,4\n5", 15]
         }
 
         for name, (arg, expected) in data.items():
