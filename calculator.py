2c69f4f9597fc01d5816a2554f6697a06e8a1d1a
Marcin Wrotecki <marcin.wrotecki.97@gmail.com>
2020-03-31 20:08:14 +0200

    [REFACTOR] Refactor add method

--- calculator.py
+++ calculator.py
@@ -1,7 +1,11 @@
 class Calculator:
     def add(self, numbers: str) -> int:
+
         if numbers.startswith("//"):
-            numbers = numbers.replace("//", ",").replace(";", ",")
+            delimiter = ["//", ";"]
+            for i in delimiter:
+                numbers = numbers.replace(i, ",")
+
         split_numbers = numbers.replace("\n", ',').split(",")
         split_numbers = list(filter(None, split_numbers))
         split_numbers = list(map(int, split_numbers))
