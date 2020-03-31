eee09864e39c6a6fe5dc68ae8a71ac5d34d2d6fa
Marcin Wrotecki <marcin.wrotecki.97@gmail.com>
2020-03-31 19:49:23 +0200

    [RED] Add test_add_adds_numbers_using_new_line_delimiter_when_string_is_valid

--- calculator_test.py
+++ calculator_test.py
@@ -26,6 +26,16 @@ class MyTestCase(unittest.TestCase):
             # Assert
             self.assertEqual(out, result)
 
+    def test_add_adds_numbers_using_new_line_delimiter_when_string_is_valid(self):
+        data = ["1\n2,3", "10\n90,10\n20"]
+        expected = [6, 130]
+        # Arrange
+        calc = calculator.Calculator()
+        # Act
+        for (inp, out) in zip(data, expected):
+            result = calc.add(inp)
+            # Assert
+            self.assertEqual(out, result)
 
 if __name__ == '__main__':
     unittest.main()
