dc4cf2b4afdbefd89f60bfa146ae5358187d8938
Marcin Wrotecki <marcin.wrotecki.97@gmail.com>
2020-03-31 19:46:00 +0200

    [GREEN] Make add method passes test

--- calculator.py
+++ calculator.py
@@ -2,10 +2,8 @@ class Calculator:
     def add(self, numbers: str) -> int:
         split_numbers = numbers.split(',')
         split_numbers = list(filter(None, split_numbers))
-        if not any(split_numbers):
-            return 0
-
-        if len(split_numbers) == 1:
-            return int(split_numbers[0])
-
-        return int(split_numbers[0])+int(split_numbers[1])
+        split_numbers = list(map(int, split_numbers))
+        sum=0
+        for i in split_numbers:
+            sum+=i
+        return sum
