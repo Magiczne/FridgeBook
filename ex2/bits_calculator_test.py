8c97f1d574bb4add4739b24671b17ef0392a6bf2
Marcin Wrotecki <marcin.wrotecki.97@gmail.com>
2020-04-01 08:49:42 +0200

    [RED]Add test_count_no_of_bits_throws_exception_when_number_greater_than_255

--- ex2/bits_calculator_test.py
+++ ex2/bits_calculator_test.py
@@ -20,6 +20,19 @@ class MyTestCase(unittest.TestCase):
         # Assert
         self.assertEqual(expected, result)
 
+    @parameterized.expand([
+        ["280"],
+        ["256"],
+        ["1000"]
+    ])
+    def test_count_no_of_bits_throws_exception_when_number_greater_than_255(self, number):
+        # Arrange
+        bits_calculator = BitsCalculator()
+        # Assert
+        with self.assertRaises(Exception):
+            # Act
+            bits_calculator.count_no_of_bits_1(number)
+
 
 if __name__ == '__main__':
     unittest.main()
