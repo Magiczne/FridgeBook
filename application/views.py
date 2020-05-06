867cf414e5b07b08c285ef16d5c29c73ae99930e
pboszczyk <pboszczyk@gmail.com>
2020-05-06 21:13:00 +0200

    Created register form

--- application/views.py
+++ application/views.py
@@ -1,8 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from application.models import Note
-
+from django.contrib.auth import login, authenticate
+from .forms import RegisterForm
+from django.shortcuts import render, redirect
+from django.template import loader
 
 def index(request):
     """View function for home page of site."""
@@ -16,3 +20,20 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/application/')
+    else:
+        form = RegisterForm()
+
+    template = loader.get_template('register.html')
+    context = {'form': form}
+    return HttpResponse(template.render(context, request))
