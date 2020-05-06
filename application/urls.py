f0fa773d5a807fae4f8b917c7d577a0452e0a5af
pbartkowicz <pat.bartkowicz@gmail.com>
2020-05-07 00:18:09 +0200

    Add required login

--- application/urls.py
+++ application/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('', views.index, name='index'),
+    path(r'^accounts/login/', auth_views.LoginView.as_view(), name='login'),
 ]
