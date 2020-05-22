b9e129bf41a9695c83d2fe6e2f9fa8f5d54415aa
pbartkowicz <pat.bartkowicz@gmail.com>
2020-05-22 11:57:49 +0200

    [Pat + FR] Load configuration from json

--- manage.py
+++ manage.py
@@ -3,8 +3,13 @@
 import os
 import sys
 
+from utils.configuration_loader import load_configuration
+
 
 def main():
+    data = load_configuration('configuration.json')
+    print('Configuration:', data)
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fridge.settings')
     try:
         from django.core.management import execute_from_command_line
