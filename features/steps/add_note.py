e20e4fae3dc197b9b2b30c7ffcfc63716c9319b4
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2020-06-05 21:58:38 +0200

    [MK + MW] Visuals cleanup

--- features/steps/add_note.py
+++ features/steps/add_note.py
@@ -1,16 +1,13 @@
 from behave import given, when, then
-from application.tests.bdd.user import UserFactory
-from application.tests.bdd.note import NoteFactory
 
-import time
+from application.tests.bdd.user import UserFactory
+
 
 @given('a logged in user who is going to add a note')
 def step_impl(context):
     u = UserFactory(username='asia', email='asia@example.com')
     u.set_password('password333')
     u.save()
-
-    
 
     br = context.browser
     br.get(context.base_url + '/login/')
@@ -42,24 +39,19 @@ def step_impl(context):
     assert br.find_element_by_name('content') is not None
 
 
-
-
 @given('a logged in user who is going to add a note and see it')
 def step_impl(context):
-     u = UserFactory(username='tola', email='tola@example.com')
-     u.set_password('password333')
-     u.save()
+    u = UserFactory(username='tola', email='tola@example.com')
+    u.set_password('password333')
+    u.save()
 
-    
+    br = context.browser
+    br.get(context.base_url + '/login/')
 
-     br = context.browser
-     br.get(context.base_url + '/login/')
+    br.find_element_by_name('username').send_keys('tola')
+    br.find_element_by_name('password').send_keys('password333')
+    br.find_element_by_id('login').click()
 
-     br.find_element_by_name('username').send_keys('tola')
-     br.find_element_by_name('password').send_keys('password333')
-     br.find_element_by_id('login').click()
-
-    
 
 @when('I try to add a note with content')
 def step_impl(context):
@@ -72,7 +64,7 @@ def step_impl(context):
     br.find_element_by_name('title').send_keys('testtitle')
     br.find_element_by_name('content').send_keys('testcontent')
     br.find_element_by_id('add').click()
-    
+
 
 @then('I see new note')
 def step_impl(context):
@@ -82,6 +74,3 @@ def step_impl(context):
     assert br.find_element_by_id("note-wrapper") is not None
     assert "testtitle" in br.find_element_by_id("note-wrapper").text
     assert "testcontent" in br.find_element_by_id("note-wrapper").text
-    
-    
-
