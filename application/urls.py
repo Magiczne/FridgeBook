fd811182312d89a72cdd031e002647e99c4455c6
filip <filip.raszka3@gmail.com>
2020-06-05 20:14:41 +0200

    [FR + PB] implement delete note

--- application/urls.py
+++ application/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('notes/add/', views.add_note, name='add_note'),
     path('notes/edit/<str:note_id>', views.edit_note, name='edit_note'),
+    path('notes/delete/<str:note_id>', views.delete_note, name='delete_note'),
     path('notes/update/<str:pk>', views.update_note, name='update_note'),
     path('', views.index, name='index'),
 ]
